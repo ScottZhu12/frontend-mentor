3d6ef0905e2d9ea4f7dd94e9091cb06a84c68af4
Scott Zhu <scott391113895@gmail.com>
2022-02-26 10:51:05 -0800

    modified

--- suite-landing-page/src/components/Button/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Button = ({ btnText, btnName }) => {
-  return <button className={`btn ${btnName}`}>{btnText}</button>;
-};
-
-export default Button;
--- suite-landing-page/src/index.js
+++
@@ -1,8 +0,0 @@
-import "./index.scss";
-
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./components/App";
-
-ReactDOM.render(<App />, document.querySelector("#root"));
