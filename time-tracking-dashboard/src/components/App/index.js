e7d2617859fb87acc56b9b54d981a32d1166ca7c
ScottZhu12 <scott391113895@gmail.com>
2022-03-10 22:49:51 -0800

    updated

--- time-tracking-dashboard/src/components/App/index.js
+++ time-tracking-dashboard/src/components/App/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import PersonalCard from "../PersonalCard";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <div className="app__content">
+        <PersonalCard />
+      </div>
+    </div>
+  );
 };
 
 export default App;
