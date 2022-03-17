8ee34143a6fcd09e9a22f55882e9f2f8367b19d2
Scott Zhu <scott391113895@gmail.com>
2022-03-17 16:46:02 -0700

    updated

---
+++ github-user-search-app/src/components/App/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "../Header";
+import SearchBar from "../SearchBar";
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="app-content">
+        <Header />
+        <SearchBar />
+      </div>
+    </div>
+  );
+};
+
+export default App;
